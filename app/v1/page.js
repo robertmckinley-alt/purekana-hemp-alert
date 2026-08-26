5e4269cccde25ab9cd5917ef0dda987f52ed1706
robertmckinley-alt <robertmckinley@gmail.com>
2026-08-26 12:41:45 -0700

    Add breaking news funnel variant

---
+++ app/v1/page.js
@@ -0,0 +1,3 @@
+import Advertorial from '../components/Advertorial';
+export const metadata={title:'Breaking Consumer Alert | Hemp Rules Change November 2026'};
+export default function Page(){return <Advertorial variant="breaking"/>}
