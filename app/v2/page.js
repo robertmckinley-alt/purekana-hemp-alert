1151838bbf2da41734b01a82e7250732db96adac
robertmckinley-alt <robertmckinley@gmail.com>
2026-08-26 12:41:48 -0700

    Add consumer investigation funnel variant

---
+++ app/v2/page.js
@@ -0,0 +1,3 @@
+import Advertorial from '../components/Advertorial';
+export const metadata={title:'Special Investigation | The Hemp THC Rule Change'};
+export default function Page(){return <Advertorial variant="investigation"/>}
