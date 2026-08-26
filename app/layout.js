bb3cd8dc286b8d5cef971a16a9de39a8f687eb22
robertmckinley-alt <robertmckinley@gmail.com>
2026-08-26 12:37:57 -0700

    Add app layout

---
+++ app/layout.js
@@ -0,0 +1,15 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Consumer Alert: Federal Hemp Rules Change November 12, 2026',
+  description: 'Sponsored consumer information about the federal hemp definition change and access to hemp-derived THC products.',
+  robots: { index: false, follow: false }
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
