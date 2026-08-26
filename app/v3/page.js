ae7d390c667e6d84102f2e60f44021e91861b15f
robertmckinley-alt <robertmckinley@gmail.com>
2026-08-26 12:41:52 -0700

    Add deadline urgency funnel variant

---
+++ app/v3/page.js
@@ -0,0 +1,3 @@
+import Advertorial from '../components/Advertorial';
+export const metadata={title:'November Deadline | Check Hemp THC Availability'};
+export default function Page(){return <Advertorial variant="deadline"/>}
